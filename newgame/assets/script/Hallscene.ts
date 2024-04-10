40bf70d2316598906f1372495a744bf7323e0fb6
Stable_xf <2731045937@qq.com>
2024-04-10 18:01:34 +0800

    commit scene

---
+++ newgame/assets/script/Hallscene.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Hallscene')
+export class Hallscene extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ newgame/assets/script/Roomscene.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Roomscene')
+export class Roomscene extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
